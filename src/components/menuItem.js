848ac1c03bcaa26d628980543db501fde04555e2
Eric Tian <893227186@qq.com>
2021-07-11 13:16:14 +0800

    Set up menuItem component

--- src/components/menuItem.js
+++ src/components/menuItem.js
@@ -1,2 +1,14 @@
 import React from 'react'
 
+class MenuItem extends React.Component {
+
+    render(){
+        return (
+            <div className="MenuItem">
+
+            </div>
+        )
+    }
+}
+
+export default MenuItems
